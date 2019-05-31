c0ef4c36cacdd8c0c5aea5d0242af0a4cb2803f9
Joxe Sandoval <joxesandoval.24@gmail.com>
2019-05-30 20:42:41 -0500

    *

---
+++ db/migrate/20190530091237_create_actions.rb
@@ -0,0 +1,10 @@
+class CreateActions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :actions do |t|
+      t.string :titulo
+      t.string :descripcion
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20190530092007_addnewvaluestoaction.rb
@@ -0,0 +1,4 @@
+class Addnewvaluestoaction < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
